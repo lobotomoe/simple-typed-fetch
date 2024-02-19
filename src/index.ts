a9e89634d4303907892048c02693f8199a75db64
kigastu <kigastu@gmail.com>
2024-02-19 22:17:26 +0300

    fix: FetchWithValidationType export (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import 'isomorphic-unfetch';
 
-export {default as fetchWithValidation} from './fetchWithValidation.js';
-export {default as simpleFetch} from './simpleFetch.js';
+export { default as fetchWithValidation } from './fetchWithValidation.js';
+export { default as simpleFetch, FetchWithValidationType } from './simpleFetch.js';
